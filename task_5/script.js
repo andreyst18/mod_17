6195439b4ee237721156d5be8dfa4e415ba87803
Andrey Strelin <andrejmax@yandex.ru>
2021-06-15 17:13:42 +0400

    add resolution of task 5 item 1

---
+++ task_5/script.js
@@ -0,0 +1,26 @@
+const wsUri = 'wss://echo.websocket.org/';
+
+const inputMessage = document.getElementById('inputMessage');
+const sendMessage = document.getElementById('input_area_btn');
+const outputArea = document.getElementById('messages_area');
+
+let websocket;
+websocket = new WebSocket(wsUri);
+
+sendMessage.addEventListener('click', () => {
+    const message = inputMessage.value;
+    inputMessage.value = '';
+    writeToScreen('<span class="message">' + message + '</span>', false);
+    websocket.send(message);
+    websocket.onmessage = function() {
+        writeToScreen('<span class="message">' + message + '</span>', true);
+    };
+          
+});
+
+function writeToScreen(message, isServer) {
+    let item = document.createElement('p');
+    item.className = isServer ? 'serverMessage' : 'userMessage';
+    item.innerHTML = message;
+    outputArea.appendChild(item);
+}
