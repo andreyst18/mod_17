d7a2396d0b909950e936b11940737a879e785423
Andrey Strelin <andrejmax@yandex.ru>
2021-06-16 13:52:38 +0400

    upd task 5

--- task_5/script.js
+++ task_5/script.js
@@ -2,7 +2,11 @@ const wsUri = 'wss://echo.websocket.org/';
 
 const inputMessage = document.getElementById('inputMessage');
 const sendMessage = document.getElementById('input_area_btn');
+const sendGeo = document.getElementById('input_geo');
 const outputArea = document.getElementById('messages_area');
+
+let message;
+let isGeo;
 
 let websocket;
 websocket = new WebSocket(wsUri);
@@ -11,15 +15,31 @@ websocket.onopen = function(evt) {
     writeToScreen("CONNECTED");
   };
 
+websocket.onmessage = function() {
+    if (!isGeo) {
+        writeToScreen('<span class="message">' + message + '</span>', true);
+    }
+};
+
 sendMessage.addEventListener('click', () => {
-    const message = inputMessage.value;
+    isGeo = false;
+    message = inputMessage.value;
     inputMessage.value = '';
     writeToScreen('<span class="message">' + message + '</span>', false);
     websocket.send(message);
-    websocket.onmessage = function() {
-        writeToScreen('<span class="message">' + message + '</span>', true);
-    };
-          
+});
+
+sendGeo.addEventListener('click', () => {
+    isGeo = true;
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const coords = position;
+            const latitude = coords.latitude;
+            const longitude = coords.longitude;
+            websocket.send(latitude, longitude);
+            writeToScreen('<span class="message">' + `<a href="https://www.openstreetmap.org/#map=18/${latitude}/${longitude}" target="_blanck">Гео-локация</a>` + '</span>', false)
+        })
+    }
 });
 
 function writeToScreen(message, isServer) {
